79ef2156e2f1656ee4bc981a93f6f77eec74f253
jc0803kevin <jc0803kevin@163.com>
2019-12-26 14:29:28 +0800

    统一异常处理

--- spring-boot-file-upload/src/main/java/com/neo/controller/GlobalExceptionHandler.java
+++ spring-boot-file-upload/src/main/java/com/neo/controller/GlobalExceptionHandler.java
@@ -18,6 +18,11 @@ public class GlobalExceptionHandler {
 
     //https://jira.spring.io/browse/SPR-14651
     //4.3.5 supports RedirectAttributes redirectAttributes
+    /**
+     * @Author kevin
+     * @Description 只捕捉 @link MultipartException.class 类似的异常
+     * @Date Created on 2019/12/26 14:28
+     */
     @ExceptionHandler(MultipartException.class)
     public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
